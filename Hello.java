32fbfbf03506b99a5e4351107bb7d79dd867c463
Joshua Mitchell <86924542+ChadwickAnderson123@users.noreply.github.com>
2022-08-23 17:33:07 -0400

    My submission for Lab 1 of CMSC 255

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/**************************************************
+* Java 'Hello, World!' Program
+***************************************************
+* Joshua Mitchell
+* 8/23/2022
+* Computer Science 255-001
+***************************************************/
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Joshua Mitchell");
+        System.out.println("Java 'Hello, World!' Program");
+        System.out.println("A Simple 'Hello, World!' Exercise");
+        System.out.println("Date Created: 8/23/2022");
+        System.out.println("Computer Science 255 - 001");
+        System.out.println();
+    }
+}
